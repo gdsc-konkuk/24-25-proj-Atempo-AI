a055d0684410a3fa3c659602c8a6f9a5db081758
Chae_Y <sallychang8596@gmail.com>
2025-05-01 18:51:17 +0900

    3

--- app/hospital.py
+++ app/hospital.py
@@ -114,7 +114,8 @@ Return only a JSON object like:
   "travel_time": "X"
 }}
 
-Do not include unit(km, min), explanations, markdown, or extra text.
+Do not include distance unit (km) and travel time unit (min).
+Do not include explanations, markdown, or extra text.
 """
     try:
         response = llm.invoke(prompt)
